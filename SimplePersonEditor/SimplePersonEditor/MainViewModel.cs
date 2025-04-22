fa170d17ca60da13e71ca835bb9fd6e56b6ec7ac
Kacper Pietrzykowski <kacperpietrzykowski2000@gmail.com>
2025-04-22 17:46:24 +0200

    wiazanie danych

---
+++ SimplePersonEditor/SimplePersonEditor/MainViewModel.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input; 
+
+namespace SimplePersonEditor
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Person _selectedPerson;
+
+        public ObservableCollection<Person> People { get; set; }
+        public ObservableCollection<string> Regions { get; set; }
+
+        public Person SelectedPerson
+        {
+            get => _selectedPerson;
+            set
+            {
+                if (_selectedPerson != value)
+                {
+                    _selectedPerson = value;
+                    OnPropertyChanged();
+                    CommandManager.InvalidateRequerySuggested();
+                }
+            }
+        }
+
+        public ICommand AddPersonCommand { get; }
+        public ICommand DeletePersonCommand { get; }
+
+        public MainViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person { FirstName = "Jan", LastName = "Kowalski", Email = "jan.kowalski@example.com", DetailsVisible = true, PaymentAmount = 150.50m, Region = "Mazowieckie", AccessLevel = 50 },
+                new Person { FirstName = "Anna", LastName = "Nowak", Email = "anna.nowak@example.com", DetailsVisible = false, PaymentAmount = 0, Region = "Małopolskie", AccessLevel = 10 },
+                new Person { FirstName = "Piotr", LastName = "Wiśniewski", Email = "piotr.wisniewski@pb.edu.pl", DetailsVisible = true, PaymentAmount = 249.95m, Region = "Podlaskie", AccessLevel = 90 }
+            };
+
+
+            Regions = new ObservableCollection<string>
+            {
+                "Dolnośląskie",
+                "Kujawsko-Pomorskie",
+                "Lubelskie",
+                "Lubuskie",
+                "Łódzkie",
+                "Małopolskie",
+                "Mazowieckie",
+                "Opolskie",
+                "Podkarpackie",
+                "Podlaskie", 
+                "Pomorskie",
+                "Śląskie",
+                "Świętokrzyskie",
+                "Warmińsko-Mazurskie",
+                "Wielkopolskie",
+                "Zachodniopomorskie"
+            };
+            
+            AddPersonCommand = new RelayCommand(AddPerson);
+            DeletePersonCommand = new RelayCommand(DeletePerson, CanDeletePerson);
+            
+            if (People.Count > 0)
+            {
+                SelectedPerson = People[0];
+            }
+        }
+
+        private void AddPerson(object parameter)
+        {
+            var newPerson = new Person
+            {
+                FirstName = "Nowa",
+                LastName = "Osoba",
+                Email = "nowa.osoba@example.com",
+                DetailsVisible = false,
+                PaymentAmount = 0,
+                Region = Regions.FirstOrDefault(), 
+                AccessLevel = 0
+            };
+            People.Add(newPerson);
+            SelectedPerson = newPerson; 
+        }
+
+        private void DeletePerson(object parameter)
+        {
+            if (SelectedPerson != null)
+            {
+                People.Remove(SelectedPerson);
+                SelectedPerson = null; 
+            }
+        }
+
+    
+        private bool CanDeletePerson(object parameter)
+        {
+            return SelectedPerson != null;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SimplePersonEditor/SimplePersonEditor/Person.cs
@@ -0,0 +1,118 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimplePersonEditor;
+
+public class Person : INotifyPropertyChanged
+{
+    private string _firstName;
+    private string _lastName;
+    private string _email;
+    private bool _detailsVisible;
+    private decimal _paymentAmount;
+    private string _region;
+    private int _accessLevel;
+
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (_firstName != value)
+            {
+                _firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DisplayName)); 
+            }
+        }
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            if (_lastName != value)
+            {
+                _lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DisplayName)); 
+            }
+        }
+    }
+
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            if (_email != value)
+            {
+                _email = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DisplayName)); 
+            }
+        }
+    }
+
+    public bool DetailsVisible
+    {
+        get => _detailsVisible;
+        set
+        {
+            if (_detailsVisible != value)
+            {
+                _detailsVisible = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public decimal PaymentAmount
+    {
+        get => _paymentAmount;
+        set
+        {
+            if (_paymentAmount != value)
+            {
+                _paymentAmount = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public string Region
+    {
+        get => _region;
+        set
+        {
+            if (_region != value)
+            {
+                _region = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public int AccessLevel
+    {
+        get => _accessLevel;
+        set
+        {
+            if (_accessLevel != value)
+            {
+                _accessLevel = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    
+    public string DisplayName => $"{FirstName} {LastName} ({Email})";
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
